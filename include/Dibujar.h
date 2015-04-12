815696b4abe065ccc45cc22af2712b8c63bc1863
fmonge <fabian1mg@gmail.com>
2015-04-11 19:55:42 -0500

    Linked List
    Agrego clases para el Linked List

---
+++ include/Dibujar.h
@@ -0,0 +1,14 @@
+#ifndef DIBUJAR_H
+#define DIBUJAR_H
+
+
+class Dibujar
+{
+    public:
+        Dibujar() {}
+        virtual ~Dibujar() {}
+    protected:
+    private:
+};
+
+#endif // DIBUJAR_H
